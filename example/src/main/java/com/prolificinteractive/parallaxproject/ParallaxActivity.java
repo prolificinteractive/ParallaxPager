1a6dc57cf43e7b3eb41edb8b3f40e7f69c7b75de
Vince Mi <vinc3m1@gmail.com>
2014-08-04 17:14:13 -0700

    update build script, use fragment to prepare for testing

--- example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxActivity.java
+++ example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxActivity.java
@@ -2,28 +2,18 @@ package com.prolificinteractive.parallaxproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.prolificinteractive.parallaxpager.ParallaxContainer;
 
 public class ParallaxActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_parallax);
 
-    // find the parallax container
-    ParallaxContainer parallaxContainer = (ParallaxContainer) findViewById(R.id.parallax_container);
-
-    if (parallaxContainer != null) {
-      // specify whether pager will loop
-      parallaxContainer.setLooping(true);
-
-      // wrap the inflater and inflate children with custom attributes
-      parallaxContainer.setupChildren(getLayoutInflater(),
-          R.layout.parallax_view_1,
-          R.layout.parallax_view_2,
-          R.layout.parallax_view_3,
-          R.layout.parallax_view_4);
+    if (savedInstanceState == null) {
+      getFragmentManager().beginTransaction()
+          .add(android.R.id.content, new ParallaxFragment())
+          .commit();
     }
+
   }
 }
---
+++ example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxFragment.java
@@ -0,0 +1,34 @@
+package com.prolificinteractive.parallaxproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.prolificinteractive.parallaxpager.ParallaxContainer;
+
+public class ParallaxFragment extends Fragment {
+
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+
+    View view = inflater.inflate(R.layout.fragment_parallax, container, false);
+
+    // find the parallax container
+    ParallaxContainer parallaxContainer = (ParallaxContainer) view.findViewById(R.id.parallax_container);
+
+    if (parallaxContainer != null) {
+      // specify whether pager will loop
+      parallaxContainer.setLooping(true);
+
+      // wrap the inflater and inflate children with custom attributes
+      parallaxContainer.setupChildren(inflater,
+          R.layout.parallax_view_1,
+          R.layout.parallax_view_2,
+          R.layout.parallax_view_3,
+          R.layout.parallax_view_4);
+    }
+
+    return view;
+  }
+}
