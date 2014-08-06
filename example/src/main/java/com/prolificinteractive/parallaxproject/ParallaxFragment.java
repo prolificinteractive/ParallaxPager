6a05ee87bf36f2d0a3859933ecc76805c200abfe
Ben Lee <ben@ben.cm>
2014-08-05 17:12:12 -0700

    remove null check

--- example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxFragment.java
+++ example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxFragment.java
@@ -15,20 +15,18 @@ public class ParallaxFragment extends Fragment {
     View view = inflater.inflate(R.layout.fragment_parallax, container, false);
 
     // find the parallax container
-    ParallaxContainer parallaxContainer = (ParallaxContainer) view.findViewById(
-        R.id.parallax_container);
+    ParallaxContainer parallaxContainer =
+        (ParallaxContainer) view.findViewById(R.id.parallax_container);
 
-    if (parallaxContainer != null) {
-      // specify whether pager will loop
-      parallaxContainer.setLooping(true);
+    // specify whether pager will loop
+    parallaxContainer.setLooping(true);
 
-      // wrap the inflater and inflate children with custom attributes
-      parallaxContainer.setupChildren(inflater,
-          R.layout.parallax_view_1,
-          R.layout.parallax_view_2,
-          R.layout.parallax_view_3,
-          R.layout.parallax_view_4);
-    }
+    // wrap the inflater and inflate children with custom attributes
+    parallaxContainer.setupChildren(inflater,
+        R.layout.parallax_view_1,
+        R.layout.parallax_view_2,
+        R.layout.parallax_view_3,
+        R.layout.parallax_view_4);
 
     view.findViewById(R.id.btn_new_frag).setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
