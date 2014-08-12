64ea2a741b8aef820fa41b54dda72943f4e0358d
Ben Lee <ben@ben.cm>
2014-08-12 13:16:40 -0700

    add calligraphy

--- example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxActivity.java
+++ example/src/main/java/com/prolificinteractive/parallaxproject/ParallaxActivity.java
@@ -4,16 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import com.prolificinteractive.parallaxpager.ParallaxContextWrapper;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class ParallaxActivity extends Activity {
 
   @Override protected void attachBaseContext(Context newBase) {
-    super.attachBaseContext(new ParallaxContextWrapper(newBase));
+    super.attachBaseContext(new CalligraphyContextWrapper(new ParallaxContextWrapper(newBase)));
   }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    CalligraphyConfig.initDefault("Bitter-Bold.ttf");
 
     if (savedInstanceState == null) {
       getFragmentManager().beginTransaction()
