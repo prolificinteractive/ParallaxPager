1d64706d0c53e9858894eb4ae8fdd5527a583d11
Adam Koski <dandc87@gmail.com>
2015-02-13 16:21:05 -0800

    Straight up copy code from the Calligraphy project and bend it to our will

---
+++ parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxFactory.java
@@ -0,0 +1,64 @@
+package com.prolificinteractive.parallaxpager;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import java.util.Arrays;
+import java.util.List;
+
+public class ParallaxFactory {
+
+  private final List<OnViewCreatedListener> otherListeners;
+
+  public ParallaxFactory(OnViewCreatedListener... others) {
+    otherListeners = Arrays.asList(others);
+  }
+
+  /**
+   * Handle the created view
+   *
+   * @param view nullable.
+   * @param context shouldn't be null.
+   * @param attrs shouldn't be null.
+   * @return null if null is passed in.
+   */
+
+  public View onViewCreated(View view, Context context, AttributeSet attrs) {
+    if (view == null) {
+      return null;
+    }
+
+    onViewCreatedInternal(view, context, attrs);
+    for (OnViewCreatedListener listener : otherListeners) {
+      if (listener != null) {
+        view = listener.onViewCreated(view, context, attrs);
+      }
+    }
+    return view;
+  }
+
+  private View onViewCreatedInternal(View view, final Context context, AttributeSet attrs) {
+
+    int[] attrIds =
+        { R.attr.a_in, R.attr.a_out, R.attr.x_in, R.attr.x_out, R.attr.y_in, R.attr.y_out, };
+
+    TypedArray a = context.obtainStyledAttributes(attrs, attrIds);
+
+    if (a != null) {
+      if (a.length() > 0) {
+        ParallaxViewTag tag = new ParallaxViewTag();
+        tag.alphaIn = a.getFloat(0, 0f);
+        tag.alphaOut = a.getFloat(1, 0f);
+        tag.xIn = a.getFloat(2, 0f);
+        tag.xOut = a.getFloat(3, 0f);
+        tag.yIn = a.getFloat(4, 0f);
+        tag.yOut = a.getFloat(5, 0f);
+        view.setTag(R.id.parallax_view_tag, tag);
+      }
+      a.recycle();
+    }
+
+    return view;
+  }
+}
--- parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxLayoutInflater.java
+++ parallaxpager/src/main/java/com/prolificinteractive/parallaxpager/ParallaxLayoutInflater.java
@@ -7,30 +7,62 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import org.xmlpull.v1.XmlPullParser;
 
-public class ParallaxLayoutInflater extends LayoutInflater {
+/**
+ * Created by chris on 19/12/2013
+ * Project: Calligraphy
+ */
+class ParallaxLayoutInflater extends LayoutInflater implements CalligraphyActivityFactory {
 
-  private final LayoutInflater fallback;
-  private final OnViewCreatedListener postView;
+  private static final String[] sClassPrefixList = {
+      "android.widget.",
+      "android.webkit."
+  };
+
+  private final ParallaxFactory mParallaxFactory;
+  // Reflection Hax
   private boolean mSetPrivateFactory = false;
+  private Field mConstructorArgs = null;
 
-  protected ParallaxLayoutInflater(LayoutInflater original, Context newContext, OnViewCreatedListener postView) {
+  protected ParallaxLayoutInflater(LayoutInflater original, Context newContext,
+      ParallaxFactory factory) {
     super(original, newContext);
-    this.fallback = original;
-    this.postView = postView;
-    setUpLayoutFactory();
+    this.mParallaxFactory = factory;
+    setUpLayoutFactories();
   }
 
-  private void setUpLayoutFactory() {
-      if (getFactory2() != null && !(getFactory2() instanceof ParallaxFactory)) {
+  @Override
+  public LayoutInflater cloneInContext(Context newContext) {
+    return new ParallaxLayoutInflater(this, newContext, mParallaxFactory);
+  }
+
+  // ===
+  // Wrapping goodies
+  // ===
+
+  @Override
+  public View inflate(XmlPullParser parser, ViewGroup root, boolean attachToRoot) {
+    setPrivateFactoryInternal();
+    return super.inflate(parser, root, attachToRoot);
+  }
+
+  /**
+   * We don't want to unnecessary create/set our factories if there are none there. We try to be
+   * as lazy as possible.
+   */
+  private void setUpLayoutFactories() {
+    // If we are HC+ we get and set Factory2 otherwise we just wrap Factory1
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+      if (getFactory2() != null && !(getFactory2() instanceof WrapperFactory2)) {
         // Sets both Factory/Factory2
         setFactory2(getFactory2());
       }
-
+    }
     // We can do this as setFactory2 is used for both methods.
-    if (getFactory() != null && !(getFactory() instanceof ParallaxFactory)) {
+    if (getFactory() != null && !(getFactory() instanceof WrapperFactory)) {
       setFactory(getFactory());
     }
   }
@@ -38,8 +70,8 @@ public class ParallaxLayoutInflater extends LayoutInflater {
   @Override
   public void setFactory(Factory factory) {
     // Only set our factory and wrap calls to the Factory trying to be set!
-    if (!(factory instanceof ParallaxFactory)) {
-      super.setFactory(new ParallaxFactory(factory, fallback));
+    if (!(factory instanceof WrapperFactory)) {
+      super.setFactory(new WrapperFactory(factory, this, mParallaxFactory));
     } else {
       super.setFactory(factory);
     }
@@ -49,22 +81,18 @@ public class ParallaxLayoutInflater extends LayoutInflater {
   @TargetApi(Build.VERSION_CODES.HONEYCOMB)
   public void setFactory2(Factory2 factory2) {
     // Only set our factory and wrap calls to the Factory2 trying to be set!
-    if (!(factory2 instanceof ParallaxFactory)) {
-      super.setFactory2(new ParallaxFactory(factory2, fallback));
+    if (!(factory2 instanceof WrapperFactory2)) {
+      super.setFactory2(new WrapperFactory2(factory2, mParallaxFactory));
     } else {
       super.setFactory2(factory2);
     }
   }
 
-  @Override
-  public View inflate(XmlPullParser parser, ViewGroup root, boolean attachToRoot) {
-    setPrivateFactoryInternal();
-    return super.inflate(parser, root, attachToRoot);
-  }
-
   private void setPrivateFactoryInternal() {
     // Already tried to set the factory.
     if (mSetPrivateFactory) return;
+    // Reflection (Or Old Device) skip.
+    if (!(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)) { return; }
     // Skip if not attached to an activity.
     if (!(getContext() instanceof Factory2)) {
       mSetPrivateFactory = true;
@@ -77,112 +105,197 @@ public class ParallaxLayoutInflater extends LayoutInflater {
     if (setPrivateFactoryMethod != null) {
       ReflectionUtils.invokeMethod(this,
           setPrivateFactoryMethod,
-          new ParallaxFactory(getFactory2(), fallback)
-      );
+          new PrivateWrapperFactory2((Factory2) getContext(), this, mParallaxFactory));
     }
     mSetPrivateFactory = true;
   }
 
+  // ===
+  // LayoutInflater ViewCreators
+  // Works in order of inflation
+  // ===
+
+  /**
+   * The Activity onCreateView (PrivateFactory) is the third port of call for LayoutInflation.
+   * We opted to manual injection over aggressive reflection, this should be less fragile.
+   */
   @Override
-  public LayoutInflater cloneInContext(Context newContext) {
-    return new ParallaxLayoutInflater(this, newContext, postView);
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  public View onActivityCreateView(View parent, View view, String name, Context context,
+      AttributeSet attrs) {
+    return mParallaxFactory.onViewCreated(
+        createCustomViewInternal(parent, view, name, context, attrs), context, attrs);
   }
 
-  @Override protected View onCreateView(String name, AttributeSet attrs) throws ClassNotFoundException {
+  /**
+   * The LayoutInflater onCreateView is the fourth port of call for LayoutInflation.
+   * BUT only for none CustomViews.
+   */
+  @Override
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  protected View onCreateView(View parent, String name, AttributeSet attrs)
+      throws ClassNotFoundException {
+    return mParallaxFactory.onViewCreated(super.onCreateView(parent, name, attrs),
+        getContext(), attrs);
+  }
 
+  /**
+   * The LayoutInflater onCreateView is the fourth port of call for LayoutInflation.
+   * BUT only for none CustomViews.
+   * Basically if this method doesn't inflate the View nothing probably will.
+   */
+  @Override
+  protected View onCreateView(String name, AttributeSet attrs) throws ClassNotFoundException {
     // This mimics the {@code PhoneLayoutInflater} in the way it tries to inflate the base
     // classes, if this fails its pretty certain the app will fail at this point.
     View view = null;
     for (String prefix : sClassPrefixList) {
       try {
-        view = super.createView(name, prefix, attrs);
+        view = createView(name, prefix, attrs);
       } catch (ClassNotFoundException ignored) {
       }
     }
     // In this case we want to let the base class take a crack
     // at it.
-    if (view == null) {
-      view = super.onCreateView(name, attrs);
+    if (view == null) { view = super.onCreateView(name, attrs); }
+
+    return mParallaxFactory.onViewCreated(view, view.getContext(), attrs);
+  }
+
+  /**
+   * Nasty method to inflate custom layouts that haven't been handled else where. If this fails it
+   * will fall back through to the PhoneLayoutInflater method of inflating custom views where
+   * Calligraphy will NOT have a hook into.
+   *
+   * @param parent parent view
+   * @param view view if it has been inflated by this point, if this is not null this method
+   * just returns this value.
+   * @param name name of the thing to inflate.
+   * @param context Context to inflate by if parent is null
+   * @param attrs Attr for this view which we can steal fontPath from too.
+   * @return view or the View we inflate in here.
+   */
+  private View createCustomViewInternal(View parent, View view, String name, Context context,
+      AttributeSet attrs) {
+    // I by no means advise anyone to do this normally, but Google have locked down access to
+    // the createView() method, so we never get a callback with attributes at the end of the
+    // createViewFromTag chain (which would solve all this unnecessary rubbish).
+    // We at the very least try to optimise this as much as possible.
+    // We only call for customViews (As they are the ones that never go through onCreateView(...)).
+    // We also maintain the Field reference and make it accessible which will make a pretty
+    // significant difference to performance on Android 4.0+.
+
+    // If CustomViewCreation is off skip this.
+    if (view == null && name.indexOf('.') > -1) {
+      if (mConstructorArgs == null) {
+        mConstructorArgs = ReflectionUtils.getField(LayoutInflater.class, "mConstructorArgs");
+      }
+
+      final Object[] mConstructorArgsArr =
+          (Object[]) ReflectionUtils.getValue(mConstructorArgs, this);
+      final Object lastContext = mConstructorArgsArr[0];
+      mConstructorArgsArr[0] = parent != null ? parent.getContext() : context;
+      ReflectionUtils.setValue(mConstructorArgs, this, mConstructorArgsArr);
+      try {
+        view = createView(name, null, attrs);
+      } catch (ClassNotFoundException ignored) {
+      } finally {
+        mConstructorArgsArr[0] = lastContext;
+        ReflectionUtils.setValue(mConstructorArgs, this, mConstructorArgsArr);
+      }
     }
-
-    postView.onViewCreated(view, getContext(), attrs);
-
     return view;
   }
 
-  @Override protected View onCreateView(View parent, String name, AttributeSet attrs) throws ClassNotFoundException {
-    View view = super.onCreateView(parent, name, attrs);
-    postView.onViewCreated(super.onCreateView(parent, name, attrs), getContext(), attrs);
-    return view;
-  }
+  // ===
+  // Wrapper Factories for Pre/Post HC
+  // ===
 
-  static final String[] sClassPrefixList = {
-      "android.widget.",
-      "android.webkit.",
-      "android.view."
-  };
+  /**
+   * Factory 1 is the first port of call for LayoutInflation
+   */
+  private static class WrapperFactory implements Factory {
 
-  private class ParallaxFactory implements Factory2 {
+    private final Factory mFactory;
+    private final ParallaxLayoutInflater mInflater;
+    private final ParallaxFactory mParallaxFactory;
 
-    private final Factory factory;
-
-    private final LayoutInflater fallback;
-
-    public ParallaxFactory(Factory factory, LayoutInflater fallback) {
-      this.factory = factory;
-      this.fallback = fallback;
+    public WrapperFactory(Factory factory, ParallaxLayoutInflater inflater,
+        ParallaxFactory parallaxFactory) {
+      mFactory = factory;
+      mInflater = inflater;
+      mParallaxFactory = parallaxFactory;
     }
 
-    @Override public View onCreateView(String name, Context context, AttributeSet attrs) {
-      return onCreateView(null, name, context, attrs);
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+      if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+        return mParallaxFactory.onViewCreated(
+            mInflater.createCustomViewInternal(
+                null, mFactory.onCreateView(name, context, attrs), name, context, attrs
+            ),
+            context, attrs
+        );
+      }
+      return mParallaxFactory.onViewCreated(
+          mFactory.onCreateView(name, context, attrs),
+          context, attrs
+      );
+    }
+  }
+
+  /**
+   * Factory 2 is the second port of call for LayoutInflation
+   */
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  private static class WrapperFactory2 implements Factory2 {
+    protected final Factory2 mFactory2;
+    protected final ParallaxFactory mParallaxFactory;
+
+    public WrapperFactory2(Factory2 factory2, ParallaxFactory parallaxFactory) {
+      mFactory2 = factory2;
+      mParallaxFactory = parallaxFactory;
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+      return mParallaxFactory.onViewCreated(
+          mFactory2.onCreateView(name, context, attrs),
+          context, attrs);
     }
 
     @Override
     public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
-      View view = null;
+      return mParallaxFactory.onViewCreated(
+          mFactory2.onCreateView(parent, name, context, attrs),
+          context, attrs);
+    }
+  }
 
-      if (factory instanceof Factory2) {
-        view = ((Factory2) factory).onCreateView(parent, name, context, attrs);
-      }
+  /**
+   * Private factory is step three for Activity Inflation, this is what is attached to the
+   * Activity on HC+ devices.
+   */
+  @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+  private static class PrivateWrapperFactory2 extends WrapperFactory2 {
 
-      if (factory != null && view == null) {
-        view = factory.onCreateView(name, context, attrs);
-      }
+    private final ParallaxLayoutInflater mInflater;
 
-      if (view == null) {
-        view = createViewOrFailQuietly(name, context, attrs);
-      }
-
-      if (view != null) {
-        postView.onViewCreated(view, context, attrs);
-      }
-
-      return view;
+    public PrivateWrapperFactory2(Factory2 factory2, ParallaxLayoutInflater inflater,
+        ParallaxFactory parallaxFactory) {
+      super(factory2, parallaxFactory);
+      mInflater = inflater;
     }
 
-    protected View createViewOrFailQuietly(String name, Context context, AttributeSet attrs) {
-      if (name.contains(".")) {
-        return createViewOrFailQuietly(name, null, context, attrs);
-      }
-
-      for (final String prefix : sClassPrefixList) {
-        final View view = createViewOrFailQuietly(name, prefix, context, attrs);
-
-        if (view != null) {
-          return view;
-        }
-      }
-
-      return null;
-    }
-
-    protected View createViewOrFailQuietly(String name, String prefix, Context context,
-        AttributeSet attrs) {
-      try {
-        return fallback.createView(name, prefix, attrs);
-      } catch (Exception ignore) {
-        return null;
-      }
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+      return mParallaxFactory.onViewCreated(
+          mInflater.createCustomViewInternal(parent,
+              mFactory2.onCreateView(parent, name, context, attrs),
+              name, context, attrs
+          ),
+          context, attrs
+      );
     }
   }
 }
